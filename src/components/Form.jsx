fcf318a1ba3b39f2aaa76f494e856c1825dae028
devmagrfs <devmagrfs@gmail.com>
2022-09-21 17:22:56 +0900

    feat: tailwind css로 스타일링 변경

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 
 export default function Form({ value, setValue, handleSubmit }) {
-
   const handleChange = (e) => {
     setValue(e.target.value);
     console.log(e);
   };
 
-
   return (
-    <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex pt-2">
       <input
         type="text"
         name="value"
-        style={{ flex: "10", padding: "5px" }}
+        className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
         placeholder="해야 할 일을 입력하세요."
         value={value}
         onChange={handleChange}>
       </input>
-      <button type="submit">입력</button>
+      <button type="submit" className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200">입력</button>
     </form>
   )
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,24 +1,6 @@
 import React from 'react'
 
 export default function List({ todoData, setTodoData }) {
-
-  const btnStyle = {
-    color: "#FFFFFF",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    }
-  };
-
   const handleClick = (id) => {
     let newTodoData = todoData.filter(data => data.id !== id);
     setTodoData(newTodoData);
@@ -37,20 +19,25 @@ export default function List({ todoData, setTodoData }) {
   return (
     <div>
       {todoData.map((data) => (
-        <div style={getStyle(data.completed)} key={data.id}>
-          <p>
-            <input
-              type="checkbox"
-              defaultChecked={data.completed}
-              onChange={() => handleCompletedChange(data.id)}
-            />
-            {data.title}
-            <button
-              style={btnStyle}
-              onClick={() => handleClick(data.id)}>
-              x
-            </button>
-          </p>
+        <div key={data.id}>
+          <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+            <div className="items-center">
+              <input
+                type="checkbox"
+                className="mr-2"
+                defaultChecked={data.completed}
+                onChange={() => handleCompletedChange(data.id)}
+              />
+              <span className={data.completed && "line-through"}>{data.title}</span>
+            </div>
+            <div className="items-center">
+              <button
+                className="px-4 py-2 float-right"
+                onClick={() => handleClick(data.id)}>
+                x
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
